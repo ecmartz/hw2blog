148cbf08b958181ac5c9a6caf7c67ec9fefa1a7c
ecmartz <ecmartz.nm@gmail.com>
2012-10-03 23:53:08 -0600

    as good as I could get it

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hw2blog::Application.config.secret_token = '36e0fd0acd1671b19cc35ef2674565fbd2226f1a4b74e0f89d566393c753b9cf9a49ae2df13d0293c165fcc8ccbe7c103f1831c99b47e3d94822b7b8e20e30a4'
